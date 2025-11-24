bd3e9ac2ec0aa5d804f432495b0e835805f06a3a
Gabriel <grodrigues256@gmail.com>
2025-11-24 18:20:08 -0300

    feat(S05-#2025-11-24) implemented others operations.

--- rest-with-spring-boot-and-java/src/main/java/br/com/gabrielrodrigues07/controllers/MathController.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/gabrielrodrigues07/controllers/MathController.java
@@ -24,6 +24,70 @@ public class MathController {
         return convertToDuble(numberOne) + convertToDuble(numberTwo);
     }
 
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("One or two of the numbers provided are not in the correct format!!");
+        }
+
+        return convertToDuble(numberOne) - convertToDuble(numberTwo);
+    }
+
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("One or two of the numbers provided are not in the correct format!!");
+        }
+
+        return convertToDuble(numberOne) * convertToDuble(numberTwo);
+    }
+
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("One or two of the numbers provided are not in the correct format!!");
+        }
+
+        return convertToDuble(numberOne) / convertToDuble(numberTwo);
+    }
+
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("One or two of the numbers provided are not in the correct format!!");
+        }
+
+        return (convertToDuble(numberOne) + convertToDuble(numberTwo)) / 2;
+    }
+
+    @RequestMapping("/squareRoot/{numberOne}")
+    public Double squareRoot(
+            @PathVariable(value = "numberOne") String numberOne
+    ) {
+
+        if (!isNumeric(numberOne)) {
+            throw new UnsupportedMathOperationException("One or two of the numbers provided are not in the correct format!!");
+        }
+
+        return Math.sqrt(convertToDuble(numberOne));
+    }
+
     private Double convertToDuble(String strNumber) {
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
