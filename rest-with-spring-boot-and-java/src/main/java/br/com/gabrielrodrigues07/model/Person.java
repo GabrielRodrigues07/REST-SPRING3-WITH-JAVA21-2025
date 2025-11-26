e8f80dbe8a87f6267ae1d08bd67e6ee709d3073c
Gabriel <grodrigues256@gmail.com>
2025-11-26 08:52:05 -0300

    feat(S07-#2025-11-26) mapping person(using lombok and jpa) entity and create person repository

--- rest-with-spring-boot-and-java/src/main/java/br/com/gabrielrodrigues07/model/Person.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/gabrielrodrigues07/model/Person.java
@@ -1,62 +1,41 @@
 package br.com.gabrielrodrigues07.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "person")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@FieldNameConstants
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Person implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "firstname", nullable = false, length = 80)
     private String firstName;
+    @Column(name = "lastname", nullable = false, length = 80)
     private String lastName;
+    @Column(name = "address", nullable = false, length = 100)
     private String address;
+    @Column(name = "gender", nullable = false, length = 6)
     private String gender;
-
-    public Person() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
 
     @Override
     public boolean equals(Object o) {
