1a6fda99ed2cdf6a8aedfd98588ed11579d3bd7a
Gabriel <grodrigues256@gmail.com>
2025-12-01 08:33:35 -0300

    feat(S09-#2025-12-01) implemented dozermapper and unit tests.

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/gabrielrodrigues07/model/dto/PersonDTO.java
@@ -0,0 +1,40 @@
+package br.com.gabrielrodrigues07.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@FieldNameConstants
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PersonDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTO person)) return false;
+        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(address, person.address) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+}
