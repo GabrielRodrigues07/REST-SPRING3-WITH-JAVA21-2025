08c39e4fa589dfb59aa722ffa1ff8cc30c9ca249
Gabriel <grodrigues256@gmail.com>
2025-12-27 15:06:02 -0300

    feat(S13-#2025-12-27) using header parameter to content negotiation

--- rest-with-spring-boot-and-java/src/main/java/br/com/gabrielrodrigues07/configs/WebConfig.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/gabrielrodrigues07/configs/WebConfig.java
@@ -11,9 +11,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+        // QUERY PARAM
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+
+        // HEADER PARAM
+        configurer.favorParameter(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
