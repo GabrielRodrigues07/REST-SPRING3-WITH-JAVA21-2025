0e465ae855d4cce399bbe89985be1062a9ed0cc7
Gabriel <grodrigues256@gmail.com>
2025-12-27 14:46:16 -0300

    feat(S13-#2025-12-27) Implemented content negotiation via query parameter

--- rest-with-spring-boot-and-java/src/main/resources/db/migration/V1.0__Create_Table_Person.sql
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V1.0__Create_Table_Person.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `person` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(80) NOT NULL,
-  `last_name` varchar(80) NOT NULL,
+  `firstname` varchar(80) NOT NULL,
+  `lastname` varchar(80) NOT NULL,
   `address` varchar(100) NOT NULL,
   `gender` varchar(6) NOT NULL,
   PRIMARY KEY (`id`)
