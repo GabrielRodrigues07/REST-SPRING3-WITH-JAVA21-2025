4f7ec6218b950cdc5b7d8d51cada0fb24f5386df
Gabriel <grodrigues256@gmail.com>
2025-12-27 14:46:16 -0300

    feat(S13-#2025-12-27) Implemented content negotiation via query parameter

--- rest-with-spring-boot-and-java/src/main/resources/db/migration/V1.1__Populate_Table_Person.sql
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V1.1__Populate_Table_Person.sql
@@ -1,4 +1,4 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+INSERT INTO `person` (`id`, `address`, `firstname`, `gender`, `lastname`) VALUES
 	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
 	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
 	(4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
